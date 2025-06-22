353b84fbfd4a948a2c50b37a1231740cb2af197f
syntax-co <51330734+syntax-co@users.noreply.github.com>
2025-06-22 14:56:53 -0500

    quick push

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Syne+Mono&display=swap" rel="stylesheet" />
